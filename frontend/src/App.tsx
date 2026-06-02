42bbe634c2f6b89e732c9fa9627ad68c2cb4c9d9
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-02 19:59:21 +0200

    Remove unused HTML files and clean up components; add LaFamiglia page and update routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import Home from "./pages/Home";
+import LaFamiglia from "./pages/LaFamiglia";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Home />} />
-
-
+        <Route path="/la-famiglia" element={<LaFamiglia />} />
       </Routes>
     </BrowserRouter>
   );
