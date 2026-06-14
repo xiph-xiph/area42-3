8eceadab1b7c780459987d9df0c01077847448a4
Nick Brouns <398364@student.fontys.nl>
2026-06-14 11:50:46 +0200

    Bezig met winkelwagen en checkout

---
+++ frontend/src/types/CartItem.ts
@@ -0,0 +1,10 @@
+/* ========================================
+   CART ITEM
+======================================== */
+
+export interface CartItem {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+}
