afc026930ea65be2c9d60ba9e9168586bc76bbd7
Nick Brouns <398364@student.fontys.nl>
2026-06-14 13:13:01 +0200

    opschonen van fouten en bugs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -11,7 +11,7 @@ export default defineConfig([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
