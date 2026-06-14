f3b196da2cb69f2e7681d525e12d105627c6c758
Nick Brouns <398364@student.fontys.nl>
2026-06-14 13:47:47 +0200

    backend ready gemaakt/ bestelling wordt als object aangeleverd

--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -3,6 +3,7 @@
 ======================================== */
 
 import "./CheckoutPage.css";
+import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 /* ========================================
@@ -11,6 +12,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 function CheckoutPage() {
     const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+const [phone, setPhone] = useState("");
+const [pickupTime, setPickupTime] = useState("Zo snel mogelijk");
+const [remarks, setRemarks] = useState("");
 
     const { state } = useLocation();
 
@@ -21,6 +27,34 @@ const totalPrice = cart.reduce(
     total + item.quantity * item.price,
   0
 );
+
+
+const handleOrder = () => {
+    const order = {
+      customer: {
+        name,
+        phone,
+      },
+  
+      pickupTime,
+  
+      remarks,
+  
+      items: cart.map(
+        (item: {
+          id: number;
+          quantity: number;
+        }) => ({
+          productId: item.id,
+          quantity: item.quantity,
+        })
+      ),
+    };
+  
+    console.log("Bestelling:", order);
+  
+    navigate("/snackhoek/bevestiging");
+  };
 
     return (
     <main className="checkout-page">
@@ -47,17 +81,21 @@ const totalPrice = cart.reduce(
           <label>
             Naam
             <input
-              type="text"
-              placeholder="Uw naam"
-            />
+  type="text"
+  placeholder="Uw naam"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+/>
           </label>
 
           <label>
             Telefoonnummer
             <input
-              type="tel"
-              placeholder="06..."
-            />
+  type="tel"
+  placeholder="06..."
+  value={phone}
+  onChange={(e) => setPhone(e.target.value)}
+/>
           </label>
 
         </section>
@@ -71,35 +109,46 @@ const totalPrice = cart.reduce(
           <h2>Afhaaltijd</h2>
 
           <label className="radio-option">
-            <input
-              type="radio"
-              name="pickup"
-              defaultChecked
-            />
+          <input
+  type="radio"
+  name="pickup"
+  value="Zo snel mogelijk"
+  checked={pickupTime === "Zo snel mogelijk"}
+  onChange={(e) => setPickupTime(e.target.value)}
+/>
             Zo snel mogelijk
           </label>
 
           <label className="radio-option">
-            <input
-              type="radio"
-              name="pickup"
-            />
+          <input
+  type="radio"
+  name="pickup"
+  value="Zo snel mogelijk"
+  checked={pickupTime === "Zo snel mogelijk"}
+  onChange={(e) => setPickupTime(e.target.value)}
+/>
             Over 15 minuten
           </label>
 
           <label className="radio-option">
-            <input
-              type="radio"
-              name="pickup"
-            />
+          <input
+  type="radio"
+  name="pickup"
+  value="Zo snel mogelijk"
+  checked={pickupTime === "Zo snel mogelijk"}
+  onChange={(e) => setPickupTime(e.target.value)}
+/>
             Over 30 minuten
           </label>
 
           <label className="radio-option">
-            <input
-              type="radio"
-              name="pickup"
-            />
+          <input
+  type="radio"
+  name="pickup"
+  value="Zo snel mogelijk"
+  checked={pickupTime === "Zo snel mogelijk"}
+  onChange={(e) => setPickupTime(e.target.value)}
+/>
             Eigen tijd
           </label>
 
@@ -119,9 +168,11 @@ const totalPrice = cart.reduce(
           <h2>Opmerkingen</h2>
 
           <textarea
-            rows={4}
-            placeholder="Bijvoorbeeld: geen zout of extra mayonaise."
-          />
+  rows={4}
+  placeholder="Bijvoorbeeld: geen zout of extra mayonaise."
+  value={remarks}
+  onChange={(e) => setRemarks(e.target.value)}
+/>
 
         </section>
 
@@ -179,7 +230,7 @@ const totalPrice = cart.reduce(
 
 <button
   className="checkout-button"
-  onClick={() => navigate("/snackhoek/bevestiging")}
+  onClick={handleOrder}
 >
   Bestelling plaatsen
 </button>
