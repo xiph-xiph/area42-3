20439659a3a85b27c01cc7f21eab7d3c7f11f8f0
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 17:59:54 +0200

    implement auth login method

--- backend/Business/Services/AuthService.cs
+++ backend/Business/Services/AuthService.cs
@@ -6,10 +6,15 @@ using Backend_Area42_3.Repositories;
 
 namespace Backend_Area42_3.Services;
 
-public class AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher)
+public class AuthService(
+    IUserRepository userRepository,
+    IPasswordHasher passwordHasher,
+    ITokenGenerator tokenGenerator
+)
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly IPasswordHasher _passwordHasher = passwordHasher;
+    private readonly ITokenGenerator _tokenGenerator = tokenGenerator;
 
     public async Task<SuccessMessageDto> Register(RegisterDto registerDto)
     {
@@ -41,6 +46,37 @@ public class AuthService(IUserRepository userRepository, IPasswordHasher passwor
 
     public async Task<TokenDto> Login(LoginDto loginDto)
     {
-        throw new NotImplementedException();
+        User? user = await _userRepository.GetUserByEmail(loginDto.Email);
+        if (user == null)
+        {
+            return new TokenDto
+            {
+                Token = null,
+                ValidUntil = null,
+                Message = "Ongeldig e-mailadres of wachtwoord.",
+                Success = false,
+            };
+        }
+
+        bool passwordValid = _passwordHasher.VerifyPassword(loginDto.Password, user.PasswordHash);
+        if (!passwordValid)
+        {
+            return new TokenDto
+            {
+                Token = null,
+                ValidUntil = null,
+                Message = "Ongeldig e-mailadres of wachtwoord.",
+                Success = false,
+            };
+        }
+
+        string token = _tokenGenerator.GenerateToken(user.Id, user.Role);
+        return new TokenDto
+        {
+            Token = token,
+            ValidUntil = DateTime.UtcNow.AddHours(1).ToString("yyyy-MM-ddTHH:mm:ssZ"),
+            Message = "Inloggen geslaagd.",
+            Success = true,
+        };
     }
 }
