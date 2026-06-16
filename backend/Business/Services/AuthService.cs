42f8cf4505ee758b92b995baff15699f61e3a846
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 15:55:35 +0200

    implement register method

--- backend/Business/Services/AuthService.cs
+++ backend/Business/Services/AuthService.cs
@@ -1,13 +1,44 @@
 using Backend_Area42_3.DTO.Input;
 using Backend_Area42_3.DTO.Output;
+using Backend_Area42_3.Enums;
+using Backend_Area42_3.Models;
+using Backend_Area42_3.Repositories;
 
 namespace Backend_Area42_3.Services;
 
-public class AuthService()
+public class AuthService(IUserRepository userRepository)
 {
-    public async Task<SuccessMessageDto> Register(RegisterDto registerDto)
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<SuccessMessageDto> Register(
+        RegisterDto registerDto,
+        IPasswordHasher passwordHasher
+    )
     {
-        throw new NotImplementedException();
+        User? existingUser = await _userRepository.GetUserByEmail(registerDto.Email);
+        if (existingUser != null)
+        {
+            return new SuccessMessageDto
+            {
+                Success = false,
+                Message = "Gebruiker met dit e-mailadres bestaat al.",
+            };
+        }
+
+        string hashedPassword = passwordHasher.HashPassword(registerDto.Password);
+
+        await _userRepository.CreateUser(
+            new User
+            {
+                Phone = registerDto.Phone,
+                Name = registerDto.Name,
+                Role = UserRole.Customer,
+                Email = registerDto.Email,
+                PasswordHash = hashedPassword,
+            }
+        );
+
+        return new SuccessMessageDto { Success = true, Message = "Registratie geslaagd." };
     }
 
     public async Task<TokenDto> Login(LoginDto loginDto)
