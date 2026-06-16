2b71bc1403a2344958a6621f0bd4fa9c948444eb
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 13:26:00 +0200

    Voeg AuthController toe met AuthService, registratie en login method stubs

--- backend/Business/Controllers/AuthController.cs
+++ backend/Business/Controllers/AuthController.cs
@@ -1,7 +1,25 @@
+using Backend_Area42_3.DTO.Input;
+using Backend_Area42_3.DTO.Output;
+using Backend_Area42_3.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend_Area42_3.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase { }
+public class AuthController(AuthService _authService) : ControllerBase
+{
+    private readonly AuthService authService = _authService;
+
+    [HttpPost("register")]
+    public async Task<SuccessMessageDto> Register(RegisterDto registerDto)
+    {
+        return await authService.Register(registerDto);
+    }
+
+    [HttpPost("login")]
+    public async Task<TokenDto> Login(LoginDto loginDto)
+    {
+        return await authService.Login(loginDto);
+    }
+}
--- backend/Business/Services/AuthService.cs
+++ backend/Business/Services/AuthService.cs
@@ -1,3 +1,17 @@
+using Backend_Area42_3.DTO.Input;
+using Backend_Area42_3.DTO.Output;
+
 namespace Backend_Area42_3.Services;
 
-public class AuthService { }
+public class AuthService()
+{
+    public async Task<SuccessMessageDto> Register(RegisterDto registerDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TokenDto> Login(LoginDto loginDto)
+    {
+        throw new NotImplementedException();
+    }
+}
