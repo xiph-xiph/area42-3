146090793ec4570b528cc94d61a315cf323fd8c1
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 16:47:40 +0200

    implement CreateUser method

--- backend/Data/Repositories/UserRepository.cs
+++ backend/Data/Repositories/UserRepository.cs
@@ -1,12 +1,30 @@
 using Backend_Area42_3.Models;
+using Npgsql;
 
 namespace Backend_Area42_3.Repositories;
 
-public class UserRepository : IUserRepository
+public class UserRepository(NpgsqlDataSource dataSource) : IUserRepository
 {
-    public Task CreateUser(User user)
+    private readonly NpgsqlDataSource _dataSource = dataSource;
+
+    public async Task CreateUser(User user)
     {
-        throw new NotImplementedException();
+        using var connection = await _dataSource.OpenConnectionAsync();
+
+        string insertQuery =
+            @"
+                INSERT INTO users (name, email, phone, password_hash, role)
+                VALUES (@Name, @Email, @Phone, @PasswordHash, @Role::user_role);
+            ";
+
+        using var command = new NpgsqlCommand(insertQuery, connection);
+        command.Parameters.AddWithValue("@Name", user.Name);
+        command.Parameters.AddWithValue("@Email", user.Email);
+        command.Parameters.AddWithValue("@Phone", (object?)user.Phone ?? DBNull.Value);
+        command.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
+        command.Parameters.AddWithValue("@Role", user.Role.ToString().ToLowerInvariant());
+
+        await command.ExecuteNonQueryAsync();
     }
 
     public Task<User> GetUserByEmail(string email)
