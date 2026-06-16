e68bd950eaecb90cfbbd259fc8c56ff0f1e21a2d
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 13:24:44 +0200

    voeg RegisterDto en TokenDto toe

---
+++ backend/Data/DTO/Input/RegisterDto.cs
@@ -0,0 +1,12 @@
+namespace Backend_Area42_3.DTO.Input;
+
+public class RegisterDto
+{
+    public required string Name { get; set; }
+
+    public required string Phone { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string Password { get; set; }
+}
