54b6de4d2912fed890c468be075637f41ed1bef6
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 14:48:44 +0200

    Implement IUserRepository interface en update UserRepository class

--- backend/Data/Repositories/UserRepository.cs
+++ backend/Data/Repositories/UserRepository.cs
@@ -1,3 +1,21 @@
+using Backend_Area42_3.Models;
+
 namespace Backend_Area42_3.Repositories;
 
-public class UserRepository { }
+public class UserRepository : IUserRepository
+{
+    public Task CreateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
