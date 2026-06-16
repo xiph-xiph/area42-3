f10fe0cbf9e9aad6a48f0dd8460756d2eed806f7
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-16 13:25:07 +0200

    voeg required modifier toe aan Phone property in User model, volgens klassendiagram

--- backend/Data/Models/User.cs
+++ backend/Data/Models/User.cs
@@ -5,7 +5,7 @@ namespace Backend_Area42_3.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Phone { get; set; } = "";
+    public required string Phone { get; set; }
     public required string Name { get; set; }
     public required UserRole Role { get; set; }
     public required string Email { get; set; }
