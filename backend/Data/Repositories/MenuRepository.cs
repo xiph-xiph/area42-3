32101952eefdb2bee2db0067defd5778b7272394
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-18 22:50:59 +0200

    Add IMenuRepository and MenuRepository

---
+++ backend/Data/Repositories/MenuRepository.cs
@@ -0,0 +1,11 @@
+namespace Backend_Area42_3.Repositories;
+
+using Backend_Area42_3.Models;
+
+public class MenuRepository : IMenuRepository
+{
+    public async Task<List<MenuItem>> GetMenuItems()
+    {
+        throw new NotImplementedException();
+    }
+}
