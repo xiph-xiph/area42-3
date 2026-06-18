7c29b54cf0649ddd490279f2b9256f0f035c9542
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-18 22:54:32 +0200

    Implement GetMenuItems method in MenuService

--- backend/Business/Services/MenuService.cs
+++ backend/Business/Services/MenuService.cs
@@ -1,11 +1,21 @@
 namespace Backend_Area42_3.Services;
 
 using Backend_Area42_3.DTO.Output;
+using Backend_Area42_3.Models;
+using Backend_Area42_3.Repositories;
 
-public class MenuService
+public class MenuService(IMenuRepository _menuRepository)
 {
+    private readonly IMenuRepository menuRepository = _menuRepository;
+
     public async Task<MenuDto> GetMenuItems()
     {
-        throw new NotImplementedException();
+        List<MenuItem> menuItems = await menuRepository.GetMenuItems();
+        return new MenuDto
+        {
+            Success = true,
+            Message = "Menu retrieved successfully",
+            Menu = menuItems
+        };
     }
 }
