88bf5303834cca32f553259f24ddbd34ab412894
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-18 22:48:16 +0200

    Add MenuController and MenuService for menu item retrieval

---
+++ backend/Business/Services/MenuService.cs
@@ -0,0 +1,11 @@
+namespace Backend_Area42_3.Services;
+
+using Backend_Area42_3.DTO.Output;
+
+public class MenuService
+{
+    public async Task<MenuDto> GetMenuItems()
+    {
+        throw new NotImplementedException();
+    }
+}
