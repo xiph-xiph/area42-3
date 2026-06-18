db75ecd53e75bf106f8666c2bd6831be1de209f5
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-18 23:18:11 +0200

    Implement GetMenuItems method in MenuRepository

--- backend/Data/Repositories/MenuRepository.cs
+++ backend/Data/Repositories/MenuRepository.cs
@@ -1,11 +1,32 @@
 namespace Backend_Area42_3.Repositories;
 
 using Backend_Area42_3.Models;
+using Npgsql;
 
-public class MenuRepository : IMenuRepository
+public class MenuRepository(NpgsqlDataSource dataSource) : IMenuRepository
 {
+    private readonly NpgsqlDataSource dataSource = dataSource;
+
     public async Task<List<MenuItem>> GetMenuItems()
     {
-        throw new NotImplementedException();
+        using var connection = await dataSource.OpenConnectionAsync();
+
+        string query =
+            "SELECT id, price, name, description FROM menu_items";
+        using var command = new NpgsqlCommand(query, connection);
+
+        using var reader = await command.ExecuteReaderAsync();
+        var menuItems = new List<MenuItem>();
+        while (await reader.ReadAsync())
+        {
+            menuItems.Add(new MenuItem
+            {
+                Id = reader.GetInt32(0),
+                Price = reader.GetDecimal(1),
+                Name = reader.GetString(2),
+                Description = reader.GetString(3)
+            });
+        }
+        return menuItems;
     }
 }
