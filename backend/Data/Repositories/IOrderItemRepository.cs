ebe724bc3500f3fadb7dc9a6bb63cde2468f764b
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-20 16:58:09 +0200

    Refactor IOrderItemRepository and OrderItemRepository to include orderId in UpdateItem and DeleteItem methods

--- backend/Data/Repositories/IOrderItemRepository.cs
+++ backend/Data/Repositories/IOrderItemRepository.cs
@@ -5,6 +5,6 @@ namespace Backend_Area42_3.Repositories;
 public interface IOrderItemRepository
 {
     Task<int> CreateItem(int orderId, OrderItem orderItem);
-    Task<bool> UpdateItem(OrderItem orderItem);
-    Task<bool> DeleteItem(int itemId);
+    Task<bool> UpdateItem(int orderId, OrderItem orderItem);
+    Task<bool> DeleteItem(int orderId, int itemId);
 }
