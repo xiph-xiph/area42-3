b403419df4df21f43436675c3056224224ba0e60
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-21 13:57:54 +0200

    add initial authentication tests and helper methods

---
+++ backend/Tests/AuthTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Backend_Area42_3;
+using Backend_Area42_3.DTO.Output;
+using System.Net;
+
+namespace Tests;
+
+public class AuthTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient client;
+    private readonly string baseUrl = "http://localhost:5000/api/auth";
+
+    public AuthTests(WebApplicationFactory<Program> factory)
+    {
+        Environment.SetEnvironmentVariable("JwtSecretKey", "test-secret-key-has-to-be-32-characters-long-or-it-will-not-work");
+        client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Register_Returns_Ok()
+    {
+        var request = new
+        {
+            Name = Helpers.GenerateRandomName(),
+            Phone = Helpers.GenerateRandomPhoneNumber(),
+            Email = Helpers.GenerateRandomEmail(),
+            Password = Helpers.GenerateRandomPassword()
+        };
+
+        var response = await client.PostAsJsonAsync(
+                    $"{baseUrl}/register",
+                    request
+                );
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(@"{""success"":true,""message"":""Registratie geslaagd.""}", body);
+    }
+}
