3b5b333bcfea1d0aed88dd277c6dea3abd9e1a96
Mike <mike.van.pol@student.gildeopleidingen.nl>
2026-06-22 00:13:52 +0200

    Voeg Issue model toe

---
+++ backend/Data/Models/Issue.cs
@@ -0,0 +1,17 @@
+using Backend_Area42_3.Enums;
+
+namespace Backend_Area42_3.Models;
+
+public class Issue
+{
+    public Guid Id { get; set; }
+    public required int UserId { get; set; }
+    public required PriorityEnum Priority { get; set; }
+    public required CategoryEnum Catergory { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required DateTime CreationDate{ get; set; }
+    public DateTime SolvedDate { get; set; }
+    public required bool Solved { get; set; }
+
+}
