bb6ff86dcd49f670e149e81d72517395d945bbad
Pjotr <pjotrzwartenkot@gmail.com>
2026-06-21 17:13:21 +0200

    change database schema and backend menu api to comply with the data the frontend expects

--- backend/Data/Models/MenuItem.cs
+++ backend/Data/Models/MenuItem.cs
@@ -3,10 +3,9 @@ namespace Backend_Area42_3.Models;
 public class MenuItem
 {
     public required int Id { get; set; }
-
+    public required string Title { get; set; }
     public required decimal Price { get; set; }
-
-    public required string Name { get; set; }
-
-    public required string Description { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string Category { get; set; }
+    public required bool IsPopular { get; set; }
 }
