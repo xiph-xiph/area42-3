c86a711c7b3ddc6da141a7fa3ed6232d50439436
Pjotr Zwartenkot <pjotrzwartenkot@gmail.com>
2026-06-21 18:33:35 +0200

    Revert "Add static file serving and fallback to index.html in Program.cs"
    This reverts commit 4650feb32210b75c87d4ec400a676247826e161f.

--- backend/Backend-Area42-3/Program.cs
+++ backend/Backend-Area42-3/Program.cs
@@ -42,13 +42,7 @@ public class Program
 
         var app = builder.Build();
 
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
         app.MapControllers();
-
-        app.MapFallbackToFile("index.html");
 
         app.Run();
     }
